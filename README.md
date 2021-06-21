acc153d4400a1e5d1d907427bad7007ea56c181e
savchart <savchart@fit.cvut.cz>
2021-06-21 15:25:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tic_tac_toe
+
+Developed with Unreal Engine 4
---
+++ Source/tic_tac_toe/tic_tac_toe.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tic_tac_toe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tic_tac_toe, "tic_tac_toe" );
